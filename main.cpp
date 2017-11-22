3f4c702eca4e13c8988775f6448115f6a8959b18
Marcelo <mvrozanti@hotmail.com>
2017-11-22 20:53:44 -0200

    A bit faster and pointer returns to origin

--- main.cpp
+++ main.cpp
@@ -53,9 +53,7 @@ static int* GetMousePos() {
     unsigned int mask_return;
 
     Display *display = XOpenDisplay(NULL);
-    //    XSetErrorHandler(_XlibErrorHandler);
     number_of_screens = XScreenCount(display);
-    fprintf(stderr, "There are %d screens available in this X session\n", number_of_screens);
     root_windows = (Window*) malloc(sizeof (Window) * number_of_screens);
     for (i = 0; i < number_of_screens; i++) {
         root_windows[i] = XRootWindow(display, i);
@@ -68,13 +66,8 @@ static int* GetMousePos() {
             break;
         }
     }
-    //    if (result != True) {
-    //        fprintf(stderr, "No mouse found.\n");
-    //        return -1;
-    //    }
     free(root_windows);
     XCloseDisplay(display);
-    //    printf("Mouse is at (%d,%d)\n", root_x, root_y);
     return new int[2] {
         root_x, root_y
     };
@@ -88,57 +81,52 @@ static int is_same_color(int* color1, int* color2) {
 void mouseClick(int x, int y) {
     Display *d = XOpenDisplay(0);
     Window root = DefaultRootWindow(d);
+    int* origin = GetMousePos();
     XWarpPointer(d, None, root, 0, 0, 0, 0, x, y);
     XFlush(d);
-    XCloseDisplay(d);
-
-    Display *display = XOpenDisplay(NULL);
-
     XEvent event;
-
-    if (display == NULL) {
+    if (d == NULL) {
         fprintf(stderr, "Cannot initialize the display\n");
         exit(EXIT_FAILURE);
     }
-
     memset(&event, 0x00, sizeof (event));
-
     event.type = ButtonPress;
     event.xbutton.button = Button1;
     event.xbutton.same_screen = True;
-
-    XQueryPointer(display, RootWindow(display, DefaultScreen(display)), &event.xbutton.root, &event.xbutton.window, &event.xbutton.x_root, &event.xbutton.y_root, &event.xbutton.x, &event.xbutton.y, &event.xbutton.state);
-
+    XQueryPointer(d, RootWindow(d, DefaultScreen(d)), &event.xbutton.root, &event.xbutton.window, &event.xbutton.x_root, &event.xbutton.y_root, &event.xbutton.x, &event.xbutton.y, &event.xbutton.state);
     event.xbutton.subwindow = event.xbutton.window;
-
     while (event.xbutton.subwindow) {
         event.xbutton.window = event.xbutton.subwindow;
-
-        XQueryPointer(display, event.xbutton.window, &event.xbutton.root, &event.xbutton.subwindow, &event.xbutton.x_root, &event.xbutton.y_root, &event.xbutton.x, &event.xbutton.y, &event.xbutton.state);
+        XQueryPointer(d, event.xbutton.window, &event.xbutton.root, &event.xbutton.subwindow, &event.xbutton.x_root, &event.xbutton.y_root, &event.xbutton.x, &event.xbutton.y, &event.xbutton.state);
     }
-
-    if (XSendEvent(display, PointerWindow, True, 0xfff, &event) == 0) fprintf(stderr, "Error\n");
-
-    XFlush(display);
-
+    if (XSendEvent(d, PointerWindow, True, 0xfff, &event) == 0) fprintf(stderr, "Error\n");
+    XFlush(d);
     usleep(100000);
-
     event.type = ButtonRelease;
     event.xbutton.state = 0x100;
+    if (XSendEvent(d, PointerWindow, True, 0xfff, &event) == 0) fprintf(stderr, "Error\n");
+    XWarpPointer(d, None, root, 0, 0, 0, 0, origin[0], origin[1]);
+    XFlush(d);
+    XCloseDisplay(d);
+}
 
-    if (XSendEvent(display, PointerWindow, True, 0xfff, &event) == 0) fprintf(stderr, "Error\n");
+void beep() {
+    int freq = 2000; // freq in hz
+    int len = 1000; // len in ms
 
-    XFlush(display);
-
-    XCloseDisplay(display);
+    int fd = open("/dev/console", O_WRONLY);
+    ioctl(fd, KIOCSOUND, (int) (1193180 / freq));
+    usleep(1000 * len);
+    ioctl(fd, KIOCSOUND, 0);
+    close(fd);
 }
 
 static void tryAndPickMid() {
     while (true) {
         //        int* color = GetPixelAt(1659, 943);
         //        printf("%d,%d,%d", color[0], color[1], color[2]);
-        int* m_pos = GetMousePos();
-        cout << m_pos[0] << " " << m_pos[1] << endl;
+        //        int* m_pos = GetMousePos();
+        //        cout << m_pos[0] << " " << m_pos[1] << endl;
         int* radiant_pos = new int[2] {
             1616, 965
         };
@@ -154,7 +142,7 @@ static void tryAndPickMid() {
         //        int* dire_pos = new int[2] {1616, 965};
         int* radiant_color = GetPixelAt(radiant_pos[0], radiant_pos[1]);
         int* dire_color = GetPixelAt(dire_pos[0], dire_pos[1]);
-        //        printf("%d,%d,%d", radiant_color[0], radiant_color[1], radiant_color[2]);
+        printf("%d,%d,%d\n", radiant_color[0], radiant_color[1], radiant_color[2]);
         //        cout << color[0] << ", " << color[1] << ", " << color[2];
         if (is_same_color(radiant_color, team_color_radiant)) { // should approximate on eventual failure ?
             mouseClick(radiant_pos[0], radiant_pos[1]);
@@ -187,17 +175,6 @@ static void LogMe(const char * fmt, ...) {
     }
 }
 
-void beep() {
-    int freq = 2000; // freq in hz
-    int len = 1000; // len in ms
-
-    int fd = open("/dev/console", O_WRONLY);
-    ioctl(fd, KIOCSOUND, (int) (1193180 / freq));
-    usleep(1000 * len);
-    ioctl(fd, KIOCSOUND, 0);
-    close(fd);
-}
-
 static int listen_for_keys() {
     Display * display;
     char szKey[32];
@@ -221,18 +198,17 @@ static int listen_for_keys() {
                 szBit = szKey[i];
                 szBitOld = szKeyOld[i];
                 iCheck = 1;
-
                 for (int j = 0; j < 8; j++) {
                     if ((szBit & iCheck) && !(szBitOld & iCheck)) {
                         iKeyCode = i * 8 + j;
                         szKeysym = XKeycodeToKeysym(display, iKeyCode, 0);
                         szKeyString = XKeysymToString(szKeysym);
-
                         XGetInputFocus(display, &focusWin, &iReverToReturn);
                         printf("%d", iKeyCode);
+                        fflush(stdout);
                         switch (iKeyCode) {
                             case 91:
-                                beep();
+                                cout << '!';
                                 thread t(tryAndPickMid);
                                 t.detach();
                                 break;
@@ -249,7 +225,5 @@ static int listen_for_keys() {
 
 int main(int argc, char** argv) {
     listen_for_keys();
-    //    beep();
-    return 0;
 }
 
--- nbproject/Makefile-Debug.mk
+++ nbproject/Makefile-Debug.mk
@@ -35,7 +35,6 @@ OBJECTDIR=${CND_BUILDDIR}/${CND_CONF}/${CND_PLATFORM}
 
 # Object Files
 OBJECTFILES= \
-	${OBJECTDIR}/DotaItemMover.o \
 	${OBJECTDIR}/main.o
 
 
@@ -62,11 +61,6 @@ LDLIBSOPTIONS=
 ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/autodota: ${OBJECTFILES}
 	${MKDIR} -p ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}
 	${LINK.cc} -o ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/autodota ${OBJECTFILES} ${LDLIBSOPTIONS} -lX11 -lpthread
-
-${OBJECTDIR}/DotaItemMover.o: DotaItemMover.cpp
-	${MKDIR} -p ${OBJECTDIR}
-	${RM} "$@.d"
-	$(COMPILE.cc) -g -std=c++14 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/DotaItemMover.o DotaItemMover.cpp
 
 ${OBJECTDIR}/main.o: main.cpp
 	${MKDIR} -p ${OBJECTDIR}
